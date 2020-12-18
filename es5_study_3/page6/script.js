eb7c26ddc209c952e967bb0c910c2bfca16bbd46
s2012975 <bdips2012@gmail.com>
2020-12-18 23:35:23 +0900

    [add]new_lesson

---
+++ es5_study_3/page6/script.js
@@ -0,0 +1,6 @@
+var friends = ["とりずきん", "みちこ", "しょう", "ひつじ仙人"];
+
+// for文を用いて、配列の値を順にコンソールに出力してください
+for (var i = 0; i < friends.length; i ++) {
+  console.log(friends[i]);
+}
---
+++ es5_study_3/page7/script.js
@@ -0,0 +1,8 @@
+// 変数userを定義し、連想配列を代入してください
+var user = {"name": "にんじゃわんこ", "age": 14};
+
+// 連想配列userのキー"name"に対応する値をドット(.)を用いて出力してください
+console.log(user.name);
+
+// 連想配列userのキー"age"に対応する値を[ ]を用いて出力してください
+console.log(user["age"]);
