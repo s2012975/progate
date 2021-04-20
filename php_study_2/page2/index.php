ecdc88844bbf608139894b52815b68881e1b6d4f
s2012975 <bdips2012@gmail.com>
2021-04-20 23:33:55 +0900

    php_study2

---
+++ php_study_2/page2/index.php
@@ -0,0 +1,14 @@
+<?php
+// Menuクラスを定義しましょう
+class Menu {
+
+}
+
+// Menuクラスのインスタンスを作り、変数$curryに代入してください
+  $curry = new Menu();
+  $pasta = new Menu();
+
+// Menuクラスのインスタンスを作り、変数$pastaに代入してください
+
+
+?>
